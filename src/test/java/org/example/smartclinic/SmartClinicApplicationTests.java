9a287551e8894a44dee0c977e1d61670d6094cc1
Aya <wwwayayoussef199@gmail.com>
2026-01-19 02:18:02 +0200

    first
    first

---
+++ src/test/java/org/example/smartclinic/SmartClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.smartclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
